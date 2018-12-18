e5f6f229d5ec04c9585ecf3fd06735e4268d441b
Lewis Freiberg <lewi@2pk.in>
2018-12-19 00:36:43 +0800

    😑 Fix silly typo in the console log

--- code/6-zmq-listen.js
+++ code/6-zmq-listen.js
@@ -5,6 +5,7 @@
 let zmq = require('zeromq')
 let sock = zmq.socket('sub')
 
+// Connect to the devnet node's ZMQ port
 sock.connect('tcp://zmq.devnet.iota.org:5556')
 
 // Check if there is no command line argument
@@ -13,13 +14,15 @@ if (!process.argv[2]) {
   console.log('Watching all TXs on the node')
   console.log('---------------------')
   console.log('Note: If you want to watch an address for TXs use this:')
-  console.log('node 6.1-zmq-listen.js AHSAK..99WS')
+  console.log('node 6-zmq-listen.js AHSAK..99WS')
 
   // Subscribe to any TX coming in to the node
   sock.subscribe('tx')
 } else {
   console.log('Watching for transactions to this address: ' + process.argv[2])
-  console.log('Remember to send a TX to this address!!')
+  console.log(
+    'Remember to send a TX to this address & be patient it can take 30seconds for the tx appear.'
+  )
   // Subscribe to the address thats passed in via the CLI
   sock.subscribe(process.argv[2])
 }
